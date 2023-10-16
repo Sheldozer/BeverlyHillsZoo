5eda7cd0291b476e48b159bc422707e0ee2e180e
Sheldozer <125883340+Sheldozer@users.noreply.github.com>
2023-10-16 11:26:10 +0200

    added interfaces IAnimal & IMakeSound and a Elephant class

---
+++ ClassLibrary/Elephant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Elephant : IAnimal, IMakeSound // Inherit LandHabitat?
+    {
+        public string Name { get; set; }
+
+        public Elephant(string name) 
+        {
+            Name = name;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("The elephant trumpets");
+        }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
