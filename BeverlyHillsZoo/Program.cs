99c1562e910d6458df31d3c75ac5f22892bfa4f7
Julia <derpette87@gmail.com>
2023-10-17 13:45:17 +0200

    Welcome text

--- BeverlyHillsZoo/Program.cs
+++ BeverlyHillsZoo/Program.cs
@@ -1,10 +1,25 @@
 ﻿// Beverly Hills Zoo av Tobias J & Julia
 using ClassLibrary;
+using Spectre.Console;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+<<<<<<< Updated upstream
+=======
+        //hej
+
+        var table = new Table();
+        table.BorderColor<Table>(Color.Green);
+
+        table.AddColumn("[yellow1]Welcome to Beverly Hill's Zoo[/]");
+        table.Border(TableBorder.Rounded);
+        table.Centered();
+
+        AnsiConsole.Write(table);
+
+>>>>>>> Stashed changes
         List<Animal> animals = new List<Animal>();
 
         Animal elephant = new Animal("Elephant");
