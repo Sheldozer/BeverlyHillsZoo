9790b453575cec287c4fb38cd26833123bf3855f
Julia <derpette87@gmail.com>
2023-10-17 13:12:50 +0200

    Stuffs
    Testgrej bara

--- BeverlyHillsZoo/Program.cs
+++ BeverlyHillsZoo/Program.cs
@@ -1,8 +1,20 @@
 ﻿// Beverly Hills Zoo av Tobias J & Julia
+using Spectre.Console;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        var table = new Table();
+        table.BorderColor<Table>(Color.Green);
+
+        table.AddColumn("[yellow1]Welcome to Beverly Hill's Zoo :rolling_on_the_floor_laughing:[/]");
+        table.Border(TableBorder.Rounded);
+        table.Centered();
+        AnsiConsole.Write(table);
+
+        Console.ReadKey();
+
     }
 }
