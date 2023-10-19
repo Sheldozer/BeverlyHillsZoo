7733f3cb5f79ba5b4ea8b24fa4fed25f3ed335c0
Sheldozer <125883340+Sheldozer@users.noreply.github.com>
2023-10-19 09:32:28 +0200

    ZooContext

--- ClassLibrary/Data/ZooContext.cs
+++ ClassLibrary/Data/ZooContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ClassLibrary.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,29 @@ namespace ClassLibrary.Data
 {
     public class ZooContext : DbContext
     {
+        public ZooContext(DbContextOptions<ZooContext> options) : base(options)
+        { }
+
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<Visitor> Visitors { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+
+        // Tobias Connectionstring: Server=.;Database=TaskManagerV2;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;
+        // Julia Connectionstring: 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=TaskManagerV2;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            // TPH
+            modelBuilder.Entity<Animal>()
+                .HasDiscriminator<string>("AnimalType")
+                .HasValue<Water>("Water")
+                .HasValue<Land>("Land")
+                .HasValue<Air>("Air");
+        }
     }
 }
---
+++ ClassLibrary/Models/Visitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Models
+{
+    public class Visitor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
