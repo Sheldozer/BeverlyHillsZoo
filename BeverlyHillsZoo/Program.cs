d8141e623a9db9e51076d388f49c81260ee7cc76
Julia <derpette87@gmail.com>
2023-10-24 19:50:48 +0200

    Seeding of visits

--- BeverlyHillsZoo/Program.cs
+++ BeverlyHillsZoo/Program.cs
@@ -17,14 +17,19 @@ internal class Program
           .UseSqlServer("Server=DESKTOP-P4PT1M9\\SQLEXPRESS;Database=BeverlyHillsZoo;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False;")
           .Options;
 
-        using var dbContext = new ZooContext(tobiasOptions);
+        using var dbContext = new ZooContext(juliaOptions);
 
         var visitorRepo = new VisitorRepository(dbContext); // add dependecy injection
         visitorRepo.SeedingVisitorData();
         var animalRepo = new AnimalRepository(dbContext);
         animalRepo.SeedAnimals();
+        var visitsRepoSeed = new VisitRepository(dbContext);
+        visitsRepoSeed.SeedVisitsData();
+
         var visitRepo = new VisitRepository(dbContext);
         var guideRepo = new GuideRepository(dbContext);
+      
+
 
         var menuRepository = new MenuRepository(dbContext, animalRepo, visitRepo, visitorRepo, guideRepo);
         menuRepository.MainMenu();
