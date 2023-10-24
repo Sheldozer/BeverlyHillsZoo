5bb614feed47b68c0d05cb33cc36673ee1199906
Sheldozer <125883340+Sheldozer@users.noreply.github.com>
2023-10-24 10:04:03 +0200

    Added project for unit testing, added dependecy injection

--- BeverlyHillsZoo/Program.cs
+++ BeverlyHillsZoo/Program.cs
@@ -1,17 +1,23 @@
 ﻿// Beverly Hills Zoo av Tobias J & Julia
 using ClassLibrary;
+using ClassLibrary.Data;
 using ClassLibrary.Models;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        var visitorSeeding = new VisitorRepository();
-        visitorSeeding.SeedingVisitorData();
-        var animalSeeding = new AnimalRepository();
-        animalSeeding.SeedAnimals();
+        using var dbContext = new ZooContext();
 
-        var menuRepository = new MenuRepository();
+
+        var visitorRepo = new VisitorRepository(dbContext); // add dependecy injection
+        visitorRepo.SeedingVisitorData();
+        var animalRepo = new AnimalRepository(dbContext);
+        animalRepo.SeedAnimals();
+        var visitRepo = new VisitRepository(dbContext);
+        var guideRepo = new GuideRepository(dbContext);
+
+        var menuRepository = new MenuRepository(dbContext, animalRepo, visitRepo, visitorRepo, guideRepo);
         menuRepository.MainMenu();
     }
 }
