3418d90d44e8bb99dc40eb1a0d79f1f80a17d098
Julia <derpette87@gmail.com>
2023-10-24 13:40:15 +0200

    D&V visits
    Delete visits, view visits

--- BeverlyHillsZoo/Program.cs
+++ BeverlyHillsZoo/Program.cs
@@ -6,6 +6,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        var updateVisitsStatus = new VisitRepository();
+            updateVisitsStatus.ArchiveOldVisits();
+
         var visitorSeeding = new VisitorRepository();
         visitorSeeding.SeedingVisitorData();
         var animalSeeding = new AnimalRepository();
