eed49525cd06d5bba3a364cf7f73ad416164aab7
Sheldozer <125883340+Sheldozer@users.noreply.github.com>
2023-10-25 11:09:50 +0200

    Test of a visit with more than 5 visitors

--- BeverlyHillsZoo.Tests/Visits/AddVisit.cs
+++ BeverlyHillsZoo.Tests/Visits/AddVisit.cs
@@ -51,5 +51,44 @@ namespace BeverlyHillsZoo.Tests.Visits
             visit.VisitDate.Should().Be(visitDate);
             visit.VisitTimeSlot.Should().Be(visitTimeSlot);
         }
+
+        [Fact]
+        public void AddVisitTooManyVisitors_ShouldNotAddToDatabase()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ZooContext>()
+                .UseInMemoryDatabase(databaseName: "AddVisitTooManyVisitorsTest")
+                .Options;
+
+            using var context = new ZooContext(options);
+            var visitRepo = new VisitRepository(context);
+
+            var animal = new Air { Id = 2, Name = "Hawk", MaxAltitude = 100 };
+            context.Animals.Add(animal);
+
+            context.Visitors.AddRange(new List<Visitor>
+            {
+                new Visitor {Id = 1, Name = "John"},
+                new Visitor {Id = 2, Name = "Sarah"},
+                new Visitor {Id = 3, Name = "Andy"},
+                new Visitor {Id = 4, Name = "Jim"},
+                new Visitor {Id = 5, Name = "Michael"},
+                new Visitor {Id = 6, Name = "Dwight"}
+            });
+            context.SaveChanges();
+
+            var visitorIds = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var visitDate = DateTime.Now.AddDays(1);
+            var visitTimeSlot = Visit.TimeSlot.Afternoon;
+
+            // Act
+            visitRepo.AddVisit(animal.Id, visitorIds, visitDate, visitTimeSlot);
+
+            //Assert
+            context.Visits.Should().BeEmpty(); // Empty since IsValid() should be false
+
+
+
+        }
     }
 }
